60d30a326e5d2619b66a7ebea296f34fe1f2bb59
Ethan Penn <empenn54@gmail.com>
2018-11-29 10:04:53 -0600

    Added functionless csp objects

---
+++ website/scripts/algorithms/Assignment.js
@@ -0,0 +1,7 @@
+export default class Assignment {
+	constructor(){}
+
+	setAssignment(varName, value){
+		this[varName] = value;
+	}
+}
---
+++ website/scripts/csp/Instruction.js
@@ -0,0 +1,7 @@
+export default class Instruction {
+	constructor(instructionName, var1, var2){
+		this.instructionName = instructionName;
+		this.var1 = var1;
+		this.var2 = var2;
+	}
+}
