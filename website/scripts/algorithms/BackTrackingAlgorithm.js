05f07bc0e281f96a9d13c5920e3ab5f909015267
joshmartin <joshmartin0212@gmail.com>
2018-12-04 10:30:49 -0600

    updated back tracking algorithm.

--- website/scripts/algorithms/BackTrackingAlgorithm.js
+++ website/scripts/algorithms/BackTrackingAlgorithm.js
@@ -9,27 +9,52 @@ class BackTrackingAlgorithm extends AbstractAlgorith{
        for(key in variables){
            this.keys.push(key);
        } 
+       //possible solutions
+       this.possibleSolutions = {};
+       for(key in keys){
+        this.possibleSolutions[key] = [];
+       }
 
        
     }
 
     step(){
         let currentKey = keys[iteration];
-        let nextKey = keys[iteration + 1];
-        for(possibleCurrent of variables[currentKey]){
+        
+        for(key of keys){
+            if(key != currentKey){
+                for(possibleCurrent of variables[currentKey]){
+                    this.assignment.set(currentKey, possibleCurrent);
+                    for(possibleNext of variables[key]){
+                        this.assignment.set(key, possibleNext);
+                        for(constraint of this.constraints){
+                            if(constraint.isSatisfiedBy(this.assignment)){
+                                this.possibleSolutions[currentKey].push(possibleCurrent);
+                                this.possibleSolutions[possibleNext].push(possible);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        /*for(possibleCurrent of variables[currentKey]){
             this.assignment.set(currentKey, possibleCurrent);
             for(possibleNext of variables[nextKey]){
                 this.assignment.set(nextKey, possibleNext);
                 for(possibleConstraint of this.constraints){
-                    if(possibleConstraint.isSatisfiedBy(this.assignment)){
-                        this.solution[currentKey] = possibleCurrent;
-                        this.solution[nextKey] = possibleNext;
-                        this.iteration += 1;
-                        return;
+                    if(!possibleConstraint.isSatisfiedBy(this.assignment)){
+                        satisfied = false;
                     }
-                } 
+                }
+                this.solution[currentKey] = possibleCurrent;
+                this.solution[nextKey] = possibleNext;
+                this.iteration += 1;
+                return;
             }
         }
         this.iteration += 1;
+        */
     }
 }
