e8ac74ce30ff29c2e596cf5bb7d7749db67b98cd
ABuckwalter <abuckwalter@nebraska.edu>
2018-12-05 15:44:07 -0600

    Clicking confirm returns a list of variables and constraints

--- website/scripts/importPage.js
+++ website/scripts/importPage.js
@@ -1,11 +1,10 @@
 // This is the file that has the functions to create the import page for csps
 import * as ImportCSP from "./importCSP.js"
+import VariablePage from "./importVariablePage.js"
 
 export default class ImportPage {
 	constructor(){
 		this.elem = undefined;
-		this.variables = 0;
-		this.constraints = 0;
 		this._construct();
 	}
 
@@ -14,6 +13,7 @@ export default class ImportPage {
 		if(this.elem.children().length !== 0) return;
 		this._constructContainer("Variable");
 		this._constructContainer("Constraint");
+		this._constructBottomButtons();
 	}
 
 	_constructRoot(){
@@ -46,6 +46,11 @@ export default class ImportPage {
 				container.addClass("Constraint");
 				this._constructTitle("Constraints:", container);
 				this._constructAddButton("Constraint", container);
+				const constraintContainer = $("<div>");
+				constraintContainer.attr("id", "SaveConstraintContainer");
+				constraintContainer.addClass("FlexColumn");
+				constraintContainer.addClass("FlexStatic");
+				container.append(constraintContainer);
 				break;
 		}
 		this.elem.append(container);
@@ -63,18 +68,86 @@ export default class ImportPage {
 		parent.append(button);
 	}
 
+	_constructBottomButtons(){
+		const container = $("<div>");
+		container.addClass("FlexRow");
+		container.addClass("FlexStatic");
+		container.addClass("FlexCenter");
+		container.attr("style", "margin-top: auto");
+
+		const confirmButton = $("<input type=\"button\" class=\"button\" value=\"Confirm\"/>");
+		confirmButton.addClass("Button");
+		confirmButton.addClass("ButtonText");
+		confirmButton.attr("style", "width: 65%");
+		confirmButton.on("click", function(){
+			const variables = []
+			const constraints = []
+			$(".VariablesToBeImported").each((i, elem) => {
+				const variablePage = new VariablePage();
+				const variable = {
+					name: $($(elem).children()[0]).text().split("Name: ").pop(),
+					domain: variablePage._parseVariableValues($($(elem).children()[1]).text().split("Domain: ").pop())
+				}
+				variables.push(variable);
+			});
+			$(".ConstraintsToBeImported").each((i, elem) => {
+				const constraint = {
+					operation: $($(elem).children()[0]).text().split("Op: ").pop(),
+					v1: $($(elem).children()[1]).text().split("v1: ").pop(),
+					v2: $($(elem).children()[2]).text().split("v2: ").pop()
+				}
+				constraints.push(constraint);
+			});
+			console.log(variables);
+			console.log(constraints);
+			$("#ImportSpace").hide();
+		});
+		const cancelButton = $("<input type=\"button\" class=\"button\" value=\"Cancel\"/>");
+		cancelButton.addClass("Button");
+		cancelButton.addClass("ButtonText");
+		cancelButton.attr("style", "width: 30%");
+		cancelButton.on("click", () => $("#ImportSpace").hide());
+		container.append(cancelButton);
+		container.append(confirmButton);
+
+		this.elem.append(container);
+	}
+
 	importedVariable(variable){
 		const container = $("<div>");
 		container.addClass("FlexRow");
 		container.addClass("FlexStatic");
+		container.addClass("VariablesToBeImported");
 		const name = $("<div>");
 		name.html("Name: " + variable.name);
 		const domain = $("<div>");
-		domain.html(" Domain: " + variable.domain);
-
+		domain.html(" Domain: " + variable.domain.values().next().value);
+		let first = true;
+		for (let item of variable.domain){
+			if(first){ first = false; continue;}
+			domain.append(", " + item);
+		}
 		container.append(name);
 		container.append(domain);
 		$("#SaveVariableContainer").append(container);
+	}
+
+	importedConstraint(constraint){
+		const container = $("<div>");
+		container.addClass("FlexRow");
+		container.addClass("FlexStatic");
+		container.addClass("ConstraintsToBeImported");
+		const operation = $("<div>");
+		operation.html("Op: " + constraint.operation);
+		const v1 = $("<div>");
+		v1.html(" v1: " + constraint.v1);
+		const v2 = $("<div>");
+		v2.html(" v2: " + constraint.v2);
+
+		container.append(operation);
+		container.append(v1);
+		container.append(v2);
+		$("#SaveConstraintContainer").append(container); 
 	}
 
 	appendTo(elemId){
