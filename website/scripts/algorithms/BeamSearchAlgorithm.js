146a08fa76b1afa28b72f9ce270c3467b377ef2b
joshmartin <joshmartin0212@gmail.com>
2018-12-07 12:04:19 -0600

    bugfix to beamsearch

--- website/scripts/algorithms/BeamSearchAlgorithm.js
+++ website/scripts/algorithms/BeamSearchAlgorithm.js
@@ -60,7 +60,7 @@ export default class BeamSearchAlgorithm extends AbstractAlgorithm {
 							worstIndex = x;
 						}
 					}
-					kBestAssignments[worstIndex] = [tempAssign, brokenConstraints,varName]
+					if(kBestAssignments[worstIndex][1] > brokenConstraints) kBestAssignments[worstIndex] = [tempAssign, brokenConstraints,varName];
 				}
 			}
 		}
